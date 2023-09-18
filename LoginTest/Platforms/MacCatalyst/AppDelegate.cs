012b4dbb002d1c76a2002815f57fad4473b8c5ea
VinceDerks <Vince.derks2006@gmail.com>
2023-09-18 09:35:40 +0200

    Add project files.

---
+++ LoginTest/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LoginTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
