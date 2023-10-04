05f024df0a3947f9d845d091a99e1c544e21f855
Emrah2810 <emrahbalija@gmail.com>
2023-10-04 13:56:56 +0200

    update

--- LoginTest/UserPage.xaml.cs
+++ LoginTest/UserPage.xaml.cs
@@ -12,12 +12,10 @@ namespace LoginTest
 
         private async void LogoutButton_Clicked(object sender, EventArgs e)
         {
-            // Clear stored credentials from secure storage
             SecureStorage.Remove("IsLoggedIn");
             SecureStorage.Remove("Username");
             SecureStorage.Remove("Password");
 
-            // Redirect to login page
             var LoginPage = new LoginPage();
             await Navigation.PushAsync(LoginPage);
             Navigation.RemovePage(this);
