342468e0435d7e032e8cd43ad7a69b6725ab0895
VinceDerks <Vince.derks2006@gmail.com>
2023-10-06 14:39:48 +0200

    added a drawingviewpage

--- LoginTest/Pages/ArrivedPage.xaml.cs
+++ LoginTest/Pages/ArrivedPage.xaml.cs
@@ -1,11 +1,36 @@
+using Microsoft.Maui.Controls;
+using CommunityToolkit.Maui.Views;
+using CommunityToolkit.Maui.Core;
+using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using CommunityToolkit.Maui.Core.Views;
 
 namespace LoginTest
 {
     public partial class ArrivedPage : ContentPage
     {
+
+
         public ArrivedPage()
         {
             InitializeComponent();
         }
+
+        [Obsolete]
+        private async void DrawingView_DrawingLineCompleted(object sender, CommunityToolkit.Maui.Core.DrawingLineCompletedEventArgs e)
+        {
+            var stream = await SignatureEntry.GetImageStream(200,200);
+            Device.BeginInvokeOnMainThread(() => {
+                Signature.Source = ImageSource.FromStream(() => stream);
+            });
+        }
+
+        private void ClearButton_Clicked(object sender, EventArgs e)
+        {
+            SignatureEntry.Clear();
+            Signature.Source = null;
+        }
+
     }
 }
--- LoginTest/Platforms/Android/MainActivity.cs
+++ LoginTest/Platforms/Android/MainActivity.cs
@@ -1,11 +1,18 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Microsoft.Maui.Controls;
+using SkiaSharp;
+using SkiaSharp.Views.Maui;
+using SkiaSharp.Views.Maui.Controls;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace LoginTest
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+
     }
 }
