c8c71c39a81e6cb090b0babb69453c81841e7276
VinceDerks <Vince.derks2006@gmail.com>
2023-11-14 16:34:46 +0100

    added multiple route support

--- LoginTest/Views/Pages/RoutesPage.xaml.cs
+++ LoginTest/Views/Pages/RoutesPage.xaml.cs
@@ -13,11 +13,14 @@ namespace LoginTest
         private int routeId;
 
         public static ContentPage RootPage { get; set; }
-        public RoutesPage()
+
+        public RoutesPage() 
         {
             InitializeComponent();
+
         }
-        public RoutesPage(int routeId)
+
+        public RoutesPage(int routeId) : this() 
         {
             this.routeId = routeId;
             DisplayDestinations(routeId);
@@ -27,7 +30,7 @@ namespace LoginTest
         {
             string jsonData = @"[
         {
-            ""ID"": 1,
+            ""ID"": ""1"",
             ""Date"": ""2023-11-10"",
             ""City"": ""Tilburg"",
             ""Address"": ""Distelweg 52"",
@@ -37,7 +40,7 @@ namespace LoginTest
             ""Status"": ""Not completed"",
         },
         {
-            ""ID"": 2,
+            ""ID"": ""1"",
             ""Date"": ""2023-11-10"",
             ""City"": ""Varik"",
             ""Address"": ""De Geus bv"",
@@ -47,7 +50,7 @@ namespace LoginTest
             ""Status"": ""Not completed"",
         },
         {
-            ""ID"": 3,
+            ""ID"": ""1"",
             ""Date"": ""2023-11-10"",
             ""City"": ""Tilburg"",
             ""Address"": ""Oaksoft"",
@@ -58,18 +61,19 @@ namespace LoginTest
 
         }
     ]";
-
+            
             List<Destination> destinations = JsonConvert.DeserializeObject<List<Destination>>(jsonData);
-
+            
             List<Destination> filteredDestinations = destinations.Where(d => d.ID == routeId).ToList();
 
             return filteredDestinations;
+            
         }
 
         private void DisplayDestinations(int routeId)
         {
             List<Destination> routes = FetchRoutesForId(routeId);
-            lblDestinations.ItemsSource = routes; 
+            lblDestinations.ItemsSource = routes;
         }
 
         private void OnRouteTapped(object sender, EventArgs e)
